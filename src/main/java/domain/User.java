1f8a1ae695956ab6388494c004335bd61ab21e38
lkacimi <laminekacimi@gmail.com>
2017-03-07 10:22:40 -0600

    commit 2

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.util.Date;
+
+/**
+ * Created by lkacimi on 3/7/2017.
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
