b752a3ea6b66dbd9e874ea9c5c6911215798c4b0
lkacimi <laminekacimi@gmail.com>
2017-03-07 16:18:10 -0600

    commit 2

---
+++ src/main/java/test/UserTest.java
@@ -0,0 +1,54 @@
+package test;
+
+import domain.User;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lkacimi on 3/7/2017.
+ */
+public class UserTest {
+    @org.junit.Test
+    public void getId() throws Exception {
+        User user = new User();
+        assertEquals(0, (int) user.getId());
+
+    }
+
+
+
+
+
+    @org.junit.Test
+    public void getName() throws Exception {
+        User u = new User();
+        u.setName("abc");
+        assertEquals("abcd",u.getName());
+    }
+
+
+
+    @org.junit.Test
+    public void getEmail() throws Exception {
+        User user = new User();
+        user.setEmail("lam@example.com");
+        assertEquals("lam@example.com",user.getEmail());
+    }
+
+
+
+    @org.junit.Test
+    public void getBirthDate() throws Exception {
+        User user = new User();
+        Date date = new Date();
+        user.setBirthDate(date);
+        assertEquals(date,user.getBirthDate());
+
+
+    }
+
+
+
+}
